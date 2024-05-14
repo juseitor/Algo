82113f7de3a05f6690225490354b02d1a92c0c7f
vboxuser <vboxuser@Jusita.myguest.virtualbox.org>
2024-05-14 20:49:59 -0300

    como esta

--- taller3/src/main/java/aed/Fecha.java
+++ taller3/src/main/java/aed/Fecha.java
@@ -1,43 +1,70 @@
 package aed;
 
 public class Fecha {
+
+    private int _dia;
+    private int _mes;
+
     public Fecha(int dia, int mes) {
-        throw new UnsupportedOperationException("No implementada aun");
+        this._dia = dia;    
+        this._mes = mes;
     }
 
     public Fecha(Fecha fecha) {
-        throw new UnsupportedOperationException("No implementada aun");
+        this._dia = fecha.dia();
+        this._mes = fecha.mes();
     }
 
     public Integer dia() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return _dia;
     }
 
     public Integer mes() {
-        throw new UnsupportedOperationException("No implementada aun");
+        return _mes;
     }
 
-    public String toString() {
-        throw new UnsupportedOperationException("No implementada aun");
-    }
+    // public Integer dia() {
+    //     throw new UnsupportedOperationException("No implementada aun");
+    // }
+
+    // public Integer mes() {
+    //     throw new UnsupportedOperationException("No implementada aun");
+    // }
 
     @Override
-    public boolean equals(Object otra) {
-        throw new UnsupportedOperationException("No implementada aun");
+    public String toString() {
+        StringBuffer sbuffer = new StringBuffer();
+
+        sbuffer.append(this._dia);
+        sbuffer.append("/");
+        sbuffer.append(this._mes);
+
+    return sbuffer.toString();
     }
 
-    public void incrementarDia() {
-        throw new UnsupportedOperationException("No implementada aun");
-    }
+    // @Override
+    // public boolean equals(Object otra) {
+    //     throw new UnsupportedOperationException("No implementada aun");
+    // }
 
-    private int diasEnMes(int mes) {
-        int dias[] = {
-                // ene, feb, mar, abr, may, jun
-                31, 28, 31, 30, 31, 30,
-                // jul, ago, sep, oct, nov, dic
-                31, 31, 30, 31, 30, 31
-        };
-        return dias[mes - 1];
-    }
+    // public void incrementarDia() {
+    //     throw new UnsupportedOperationException("No implementada aun");
+    // }
 
+    // private int diasEnMes(int mes) {
+    //     int dias[] = {
+    //             // ene, feb, mar, abr, may, jun
+    //             31, 28, 31, 30, 31, 30,
+    //             // jul, ago, sep, oct, nov, dic
+    //             31, 31, 30, 31, 30, 31
+    //     };
+    //     return dias[mes - 1];
+    // }
+
+    public static void main(String[] args) {
+        Fecha miFecha = new Fecha(12, 5);
+        System.out.println(miFecha);
+        // System.out.println("Dia: " + miFecha.dia());
+        // System.out.println("Mes " + miFecha.mes());
+    }
 }
